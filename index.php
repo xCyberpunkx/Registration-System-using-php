5c663bce457c2c688033905d1d80133790a95d90
xCyberpunkx <116972275+xCyberpunkx@users.noreply.github.com>
2023-10-09 23:15:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$page_title = "Home Page";
+include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+              <h3>Login and Registration System in PHP</h3>  
+              <h3>With Email Verification</h3>  
+            </div>  
+        </div>
+    </div>
+</div>
+<?php include('includes/footer.php');?>
