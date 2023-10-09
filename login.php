b39df96ff4cf22a757447bd6bd17960d27547b74
xCyberpunkx <116972275+xCyberpunkx@users.noreply.github.com>
2023-10-09 23:14:56 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php 
+$page_title = "Login Form";
+include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header ">
+                        <h5>Login Form</h5>
+                    </div>
+                    <div class="card-body ">
+                        <form action="">
+                            <div class="form-group mb-3">
+                             
+                            <div class="form-group mb-3">
+                                <label for="">Email Address</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Confirm password</label>
+                                <input type="password" name="confirm_pasdword" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php');?>
