80e004aaa9a6e129bd5b5e186cfb5b0525430e7a
xCyberpunkx <116972275+xCyberpunkx@users.noreply.github.com>
2023-10-09 23:15:24 +0100

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<?php
+$page_title = "Dashboard";
+include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>User Dashboard</h4>
+                    </div>
+                    <div class="card-body">
+                    <h3>Access when you are Logged In</h3>  
+                     
+                    </div>
+                </div>
+              
+            </div>  
+        </div>
+    </div>
+</div>
+<?php include('includes/footer.php');?>
