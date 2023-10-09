825e33b465f011f6c65e605baa64b622072ee0cb
xCyberpunkx <116972275+xCyberpunkx@users.noreply.github.com>
2023-10-09 23:16:49 +0100

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,35 @@
+<div class="bg-dark">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="row">
+
+          <div class="col-md-12 text-center">
+            <a class="navbar-brand" href="#">Web Weaver</a>
+          </div>
+        </div>
+
+        <!-- Navigation links with responsive behavior -->
+        
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">Dashboard</a>
+            </li>
+          </ul>
+        
+
+      </nav>
+    </div>
+  </div>
+</div>
+</div>
